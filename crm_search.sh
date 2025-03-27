b1532c8389b55cdb2f21f3cc23c65ab457796d68
yurisuki <adamnvrtil@icloud.com>
2025-03-27 11:15:00 +0100

    crm search added

---
+++ crm_search.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# This tool will search on ZOHO CRM.
+# Requirements: rofi
+
+## VARIABLES
+# Organization number
+org=(org20063443645)
+
+# Use Rofi to get user input for the search query
+query=$(rofi -dmenu -p "Enter search query:")
+
+# Check if the user pressed ESC or provided empty input
+if [ -z "$query" ]; then
+    echo "No input provided. Exiting..."
+    exit 1
+fi
+
+# Replace spaces with URL encoding for spaces (%20)
+encoded_query=$(echo "$query" | sed 's/ /%20/g')
+
+# Build the Zoho CRM search URL
+url="https://crm.zoho.eu/crm/$org/search?searchword=$encoded_query&isRelevance=false"
+
+# Open the URL in the default web browser
+xdg-open "$url"
